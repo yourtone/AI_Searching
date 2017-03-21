ed677d6060d4d92f5189b305f8c17a0e2acf474e
yourtone <yuetanlin@gmail.com>
2017-03-21 16:17:05 +0800

    IDS and UCS search algorithms added

--- test_puzzle8.py
+++ test_puzzle8.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+__author__  = "Yuetan Lin"
+__version__ = "1.2.1"
+
 from Search import *
 import numpy as np
 
@@ -9,11 +13,6 @@ class State: # [problem specified]
         self.SIZE = SIZE
     def create(self, arr):
         return np.uint8(np.resize(np.array(arr),self.SIZE))
-
-'''
-def create_state(arr, size=PZ8SZ):
-    return np.uint8(np.resize(np.array(arr),size))
-'''
 
 class Problem:
     def __init__(self, INITIAL_STATE, GOAL_STATE, ALL_ACTIONS, SIZE):
@@ -31,13 +30,12 @@ class Problem:
     def ACTIONS(self, state): # [problem specified]
         l1 = self.FIND_ZERO(state)
         feaaction = [] # feasible actions
-        for action, actposi in self.ALL_ACTIONS.iteritems():
-            l2 = l1 + actposi # l2: new_location
-            if self.IN_BOUNDS(l2):
+        for action, actdirection in self.ALL_ACTIONS.iteritems():
+            if self.IN_BOUNDS(l1 + actdirection):
                 feaaction.append(action)
         return feaaction
     def RESULT(self, p_state, action): # [problem specified]
-        l1 = self.FIND_ZERO(state)
+        l1 = self.FIND_ZERO(p_state)
         l2 = l1 + self.ALL_ACTIONS[action] # l2: new_location
         #if not self.IN_BOUNDS(l2):
         #    print 'Never go through'
@@ -71,11 +69,6 @@ actions = {'up':(-1,0), 'right':(0,1), 'down':(1,0), 'left':(0,-1)}
 # puzzle 3
 size = PZ3SZ
 state3 = State(size)
-#init_state = state3.create([1,0,2,3]) # BFS & DFS need 1 step, DLS needs 11 steps
-#init_state = state3.create([0,2,3,1]) # BFS & DFS need 4 steps, DLS needs 8 steps
-#init_state = state3.create([1,3,0,2]) # BFS & DLS need 3 steps, DFS needs 9 steps
-#init_state = state3.create([3,1,2,0]) # failure
-#init_state = state3.create([3,0,1,2])
 init_state = state3.create(np.random.permutation(np.arange(np.prod(size))))
 goal_state = state3.create(np.arange(np.prod(size)))
 
@@ -84,7 +77,6 @@ puzzle3 = Problem(init_state, goal_state, actions, size)
 ### ================ ###
 ###      Search      ###
 ### ================ ###
-'''
 print '===Puzzle 3==='
 print '---BFS---'
 solution = BREADTH_FIRST_SEARCH(puzzle3)
@@ -95,47 +87,8 @@ solution = DEPTH_FIRST_SEARCH(puzzle3)
 PRINT_SOLUTION(solution)
 
 print '---DLS---'
-solution = DEPTH_LIMITED_SEARCH_2(puzzle3, 15) # compare BFS and DLS for [3,0,1,2]
+solution = DEPTH_LIMITED_SEARCH_2(puzzle3, 10)
 PRINT_SOLUTION(solution, True)
-'''
-
-'''
-state_list = []
-totalNum = np.prod(size)
-for l1 in range(totalNum):
-    state_list.append(l1)
-    for l2 in range(totalNum):
-        if l2 in state_list:
-            continue
-        state_list.append(l2)
-        for l3 in range(totalNum):
-            if l3 in state_list:
-                continue
-            state_list.append(l3)
-            for l4 in range(totalNum):
-                if l4 in state_list:
-                    continue
-                state_list.append(l4)
-                init_state = state3.create([l1,l2,l3,l4])
-                print init_state
-                puzzle3 = Problem(init_state, goal_state, actions, size)
-                print '---BFS---'
-                solution = BREADTH_FIRST_SEARCH(puzzle3)
-                PRINT_SOLUTION(solution)
-
-                print '---DFS---'
-                solution = DEPTH_FIRST_SEARCH(puzzle3)
-                PRINT_SOLUTION(solution)
-
-                print '---DLS---'
-                solution = DEPTH_LIMITED_SEARCH_2(puzzle3, 15)
-                PRINT_SOLUTION(solution, True)
-                raw_input()
-                state_list.pop()
-            state_list.pop()
-        state_list.pop()
-    state_list.pop()
-'''
 
 
 ### ================ ###
@@ -144,15 +97,8 @@ for l1 in range(totalNum):
 # puzzle 8
 size = PZ8SZ
 state8 = State(size)
-#init_state = state8.create([7,2,4,5,0,6,8,3,1])
-#init_state = state8.create([1,2,0,3,4,5,6,7,8]) # 2 steps
-#init_state = state8.create([1,2,5,3,4,0,6,7,8]) # 3 steps
-init_state = state8.create([3,1,2,4,5,8,6,0,7]) # 5 steps
-#init_state = state8.create([3,1,2,6,4,8,0,5,7]) # 8 steps
-#init_state = state8.create([3,2,5,4,1,0,6,7,8]) # 5 steps
-#init_state = state8.create([1,0,2,6,4,8,7,5,3]) # 11 stesp
-#init_state = state8.create([0,2,7,3,8,1,4,6,5]) # 14 steps
-#init_state = state8.create([1,0,3,2,4,8,7,5,6]) #  steps
+init_state = state8.create([1,0,2,6,4,8,7,5,3]) # 11 stesp
+init_state = state8.create([7,2,4,5,0,6,8,3,1])
 #init_state = state8.create(np.random.permutation(np.arange(np.prod(size))))
 goal_state = state8.create(np.arange(np.prod(size)))
 
@@ -162,20 +108,22 @@ puzzle8 = Problem(init_state, goal_state, actions, size)
 ###      Search      ###
 ### ================ ###
 print '===Puzzle 8==='
-print '---BFS---'
-solution = BREADTH_FIRST_SEARCH(puzzle8)
-PRINT_SOLUTION(solution)
+#print '---BFS---'
+#solution = BREADTH_FIRST_SEARCH(puzzle8)
+#PRINT_SOLUTION(solution)
+
+#print '---UCS---'
+#solution = UNIFORM_COST_SEARCH(puzzle8)
+#PRINT_SOLUTION(solution)
 
 #print '---DFS---'
 #solution = DEPTH_FIRST_SEARCH(puzzle8)
 #PRINT_SOLUTION(solution)
 
-print '---DLS---'
-solution = DEPTH_LIMITED_SEARCH_2(puzzle8, 12) #compare 10 and 8 for [3,1,2,6,4,8,0,5,7]
+#print '---DLS---'
+#solution = DEPTH_LIMITED_SEARCH_2(puzzle8, 12)
+#PRINT_SOLUTION(solution, True)
+
+print '---IDS---'
+solution = ITERATIVE_DEEPENING_SEARCH(puzzle8)
 PRINT_SOLUTION(solution, True)
-
-
-'''
-print '---A*---'
-
-'''
